ce473983bfa037a151c8f787eef512de6f19e2ea
chinky23-tech <chinky0330@gmail.com>
2025-10-29 13:33:21 +0530

    auth

--- server/model/User.js
+++ server/model/User.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt js');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
 email: { type: String, required: true, unique:true},
 password: {type: String, required: true},
 name: {type: String, required: true}
 },{timestamps: true});
+// hash password
 
-//password hashing middleware
+userSchema.pre('save' , async function(next) {
+    if(!this.isModified('password')) return next();
+    
+    this.password = await bcrypt.hash(this.password, 12); //12 salt rounds
+    next();
+});
+// security compare pasword for login
+userSchema.methods.correctPassword = async function (candidatePassowrd, userPassword) {
+    
+    return await bcrypt.compare(candidatePassowrd, userPassword);
+}
 
 module.exports = mongoose.model('User', userSchema);
